dd1d97073585a2a9f45c956663b05b305353e37b
mynirka1234 <mahmutovamunira0@gmail.com>
2026-02-25 06:23:01 +0500

    добавление ключей капчи Махмутова

--- login.php
+++ login.php
@@ -19,6 +19,7 @@
 		<title> Авторизация </title>
 		
 		<script src="https://code.jquery.com/jquery-1.8.3.js"></script>
+		<script src="https://www.google.com/recaptcha/api.js"></script>
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
@@ -41,6 +42,9 @@
 					<input name="_login" type="text" placeholder="" onkeypress="return PressToEnter(event)"/>
 					<div class = "sub-name">Пароль:</div>
 					<input name="_password" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
+					<center>
+						<div class="g-recaptcha" data-sitekey="6Lev6nYsAAAAAHT-hY6swtQbiZjZUPHfdZIIgQ2K"></div>
+					</center>
 					
 					<a href="regin.php">Регистрация</a>
 					<br><a href="recovery.php">Забыли пароль?</a>
@@ -58,16 +62,25 @@
 		
 		<script>
 			function LogIn() {
+				
 				var loading = document.getElementsByClassName("loading")[0];
 				var button = document.getElementsByClassName("button")[0];
-				
 				var _login = document.getElementsByName("_login")[0].value;
 				var _password = document.getElementsByName("_password")[0].value;
+                
+                if(_login == ""){
+                    alert("Введите логин.");
+                    return;
+                }
+                if(_password == ""){
+                    alert("Введите пароль.");
+                    return;
+                }
 				loading.style.display = "block";
-				button.className = "button_diactive";
-				
+                button.className = "button_diactive";
+
 				var data = new FormData();
-				data.append("login", _login);
+                data.append("login", _login);
 				data.append("password", _password);
 				
 				// AJAX запрос
